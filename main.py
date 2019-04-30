8ef6d7efe0e4227b16c48755a5cc94221c9a2cbc
Divyaja Kakarla <divyakakarla93@gmail.com>
2019-04-30 12:56:43 +0530

    Update main.py
    Added rendering to html and get and post methods

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,request
 
 app=Flask(__name__)
+@app.route("/success/<name>")
+def success(name):
+    return "Welcome to Flask Module %s" % name.upper()
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method=='POST':
+        username=request.form['uname']
+        return redirect(url_for('success',name=username))
+    else:
+        username=request.args.get('uname')
+        return redirect(url_for('success',name=username))
 
 @app.route("/")
 def home():
  return "Hello Divya!"
+
+@app.route('/')
+def home():
+ return render_template('Home.html')
+
 
 if __name__=="__main__":
  app.run(debug=True)
